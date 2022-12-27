54506d6c7e2c79939ec27e81a0064319de480100
sivasai9 <sivasaikrishna.sankranthi16@gmail.com>
2022-12-27 20:03:59 +0530

    fhttps://github.com/sivasai9/demo_push

---
+++ basic.java
@@ -0,0 +1,13 @@
+import stacks.stack;
+
+public class basic{
+    public static void main(String[] args){
+
+        
+
+
+
+        
+    }
+    
+}
---
+++ stacks/stack.java
@@ -0,0 +1,14 @@
+package stacks;
+import java.util.*;
+
+public class stack {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the number:");
+        sc.nextInt();
+
+
+    }
+
+    
+}
